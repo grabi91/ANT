872df77ea6b4ad6a6dddadcca14f84bfc744e64b
grabi <lukasz.graban@gmail.com>
2017-09-14 01:52:19 +0200

    ADC comments

--- Atmega2560_Rs232/Atmega2560_Rs232/ADC/ADC.c
+++ Atmega2560_Rs232/Atmega2560_Rs232/ADC/ADC.c
@@ -6,62 +6,67 @@
  */
 #include "ADC.h"
 
-#define ADC_VREF 4.53
+#define ADC_VREF 4.76
 #define ADC_MULTIPLE_1 1
 #define ADC_MULTIPLE_2 2
 #define ADC_MULTIPLE ADC_MULTIPLE_1
 
+// Funkcja inicjuje konwerter analogowo cyfrowy ADC
 void ADC_Init()
 {
-	// AREF = AVcc
+   // Wybór odpowiedniego napiêcia referencjnego w zale¿noœci od tego czy
+   // zosta³ u¿yty dzielnik napiêcia
    if(ADC_MULTIPLE == ADC_MULTIPLE_2)
    {
-	   ADMUX = (1<<REFS0)|(1<<REFS1);
+      // AREF = 2,56V
+      ADMUX = (1<<REFS0)|(1<<REFS1);
    }
    else if(ADC_MULTIPLE == ADC_MULTIPLE_1)
    {
+      // AREF = AVcc czyli 5V
       ADMUX = (1<<REFS0);
-   }            
-	
-	// ADC Enable and prescaler of 128
-	// 16000000/128 = 125000
-	ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+   }
+
+   // Uruchomienie ADC i ustawienie prescalera na wartoœæ 128
+   // otrzymujemy waroœæ czêstotliwoœci 16000000/128 = 125000
+   ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
 }
 
+//Funckja odczytuje wartoœæ napiecia dla danego kana³y
 STATUS ADC_Read(IN ADC_CHANNEL Channel, OUT ADC_RESPONSE *pADC)
 {
-	if(Channel >= 8)
-	{
-		Channel	 &= 7;
-		
-		ADCSRB |= (1<<MUX5);
-		ADMUX = (ADMUX & 0xF8)|Channel; // clears the bottom 3 bits before ORing
-	}
-	else
-	{
-		ADCSRB &= ~(1<<MUX5);
-		ADMUX = (ADMUX & 0xF8)|Channel; // clears the bottom 3 bits before ORing		
-	}
-	
-	// start single convertion
-	// write ’1? to ADSC
-	ADCSRA |= (1<<ADSC);
-	
-	// wait for conversion to complete
-	// ADSC becomes ’0? again
-	// till then, run loop continuously
-	while(ADCSRA & (1<<ADSC));
-	
-	*pADC = ADC;
-	return (STATUS_SUCCESS);
+   if(Channel >= 8)
+   {
+      Channel   &= 7;
+
+      ADCSRB |= (1<<MUX5);
+      ADMUX = (ADMUX & 0xF8)|Channel; // Wyzerowanie najni¿szych 3 bitów
+   }
+   else
+   {
+      ADCSRB &= ~(1<<MUX5);
+      ADMUX = (ADMUX & 0xF8)|Channel; // Wyzerowanie najni¿szych 3 bitóws
+   }
+
+   // rozpoczêcie pojedyñczej transakcji
+   // wpisanie 1 do ADSC
+   ADCSRA |= (1<<ADSC);
+
+   // jeœli konwersja siê zakoñczy
+   // ADSC bedzie mia³o spowrotem waroœæ 0
+   while(ADCSRA & (1<<ADSC));
+
+   *pADC = ADC;
+   return (STATUS_SUCCESS);
 }
 
+// Funkcja zamienia wartoœæ odczytan¹ z ADC na konkretne napiêcie
 STATUS ADC_ValueToVoltage(ADC_RESPONSE Adc, float *pVoltage)
 {
    STATUS Status = STATUS_SUCCESS;
    float Temp = Adc;
-   
+
    *pVoltage = (Temp / 1024) * ADC_VREF * ADC_MULTIPLE;
-   
+
    return Status;
 }
